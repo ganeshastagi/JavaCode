991325c6b35ccd21c7bdc306b7a8b2356d538df7
ganeshastagi <Ganeshastagi6@gmail.com>
2025-08-01 18:11:58 -0400

    Age Checker

---
+++ AgeChecker.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class AgeChecker {
+    public static void main (String[] args) {
+        int age;
+        Scanner sc = new Scanner(System.in);
+        System.out.println ("Please enter your age: ");
+        age = sc.nextInt();
+        if(age>=18)
+        {
+            System.out.println("You're an Adult! Go get a Job, idiot!");
+        }
+        else {
+            System.out.println("Get out of your mother basement.");
+        }
+    }
+}
