aee13cea0f5749056ece3ec9f06214fe162d87ce
ganeshastagi <Ganeshastagi6@gmail.com>
2025-08-01 17:43:44 -0400

    Even numbers from 0 to 100

---
+++ Even.java
@@ -0,0 +1,10 @@
+public class Even {
+    public static void main (String [] args) {
+        // Print Even numbers from 0 to 100
+        for(int i=1 ; i<=100 ; i++) {
+            if (i%2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
